84679e64e2d24eb802eec99f601df13a489551f6
ashkan <ashkan.nemati@gmail.com>
2018-01-29 16:11:37 +0800

    Release to staging

--- js/manifest.js
+++ js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"6c36e8fa2872529b6538","1":"46411d9885d0132bc18e","2":"2c87e291d970a38c901b","3":"fc65bd8ee2b4ed6278fd"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"6c36e8fa2872529b6538","1":"46411d9885d0132bc18e","2":"05080893df1abe01dc8d","3":"fc65bd8ee2b4ed6278fd"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
