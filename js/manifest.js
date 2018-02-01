bcd0cd9bb03e2ca6f0e653caafcc662da3872af0
ashkan <ashkan.nemati@gmail.com>
2018-02-01 10:02:37 +0800

    Release to staging

--- js/manifest.js
+++ js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"6c36e8fa2872529b6538","1":"59a20e2c69e5dacc4512","2":"95d4bbfdf823d89c6ed7","3":"fc65bd8ee2b4ed6278fd"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"6c36e8fa2872529b6538","1":"59a20e2c69e5dacc4512","2":"88775e0b0a26c0daad1b","3":"fc65bd8ee2b4ed6278fd"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
