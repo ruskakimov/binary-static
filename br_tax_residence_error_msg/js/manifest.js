8c8fce89f97168bc18d72776912a2627814d459b
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-29 16:43:06 +0800

    Deploy to tax_residence_error_msg

--- br_tax_residence_error_msg/js/manifest.js
+++ br_tax_residence_error_msg/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"dd60902a4d42c3937214","1":"ac89d1ae311194f61256","2":"cde35e6d162e07bbae4a","3":"e98330b0825e8ecffa71"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"dd60902a4d42c3937214","1":"ac89d1ae311194f61256","2":"0676232389ba5af73508","3":"e98330b0825e8ecffa71"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
