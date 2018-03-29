562963aea29aba1a6bc2c45cf6cdc29b88eb44b0
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-29 16:10:52 +0800

    Deploy to tax_residence_error_msg

--- br_tax_residence_error_msg/js/binary.js
+++ br_tax_residence_error_msg/js/binary.js
@@ -4015,7 +4015,7 @@ module.exports = Lookback;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = 12356; // you can insert Application ID of your registered application here
+    var user_app_id = '12356'; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     if (config_app_id) {
         app_id = config_app_id;
--- br_tax_residence_error_msg/js/manifest.js
+++ br_tax_residence_error_msg/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"dd60902a4d42c3937214","1":"ac89d1ae311194f61256","2":"82881da136df3f284364","3":"e98330b0825e8ecffa71"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"dd60902a4d42c3937214","1":"ac89d1ae311194f61256","2":"cde35e6d162e07bbae4a","3":"e98330b0825e8ecffa71"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
