7a9e0fb95b687b1fbc0b2ebbe8a6d468b9977b3e
John Baek <jbaek7023@gmail.com>
2018-06-12 09:35:33 +0800

    added Cashier UI + updated submit button component

--- src/javascript/app_2/pages/settings/sections/cashier_password.jsx
+++ src/javascript/app_2/pages/settings/sections/cashier_password.jsx
@@ -1,13 +1,41 @@
 import React, { PureComponent } from 'react';
 import { SettingContentHeader } from '../components/setting_content_header.jsx';
+import { FormFieldSet } from '../components/form_field_set.jsx';
+import { FormSubmitButton } from '../components/form_submit_button.jsx';
 
 class CashierPassword extends PureComponent {
+    state = {
+            cashier_pw: '',
+            verified_cashier_pw: '',
+    }
+
+    onChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({[name]: value})
+    }
+
     render() {
         const { title, content } = this.props;
+        const { cashier_pw, verified_cashier_pw } = this.state;
         return (
             <div className='settings__content_container'>
                 <SettingContentHeader title={title} content={content}/>
-                <div className='settings__content_form_container'></div>
+                <div className='settings__content_form__container'>
+                    <FormFieldSet
+                        label_name='Current Password'
+                        value={cashier_pw}
+                        onChange={this.onChange}
+                        onPasswordVisibilityClick={this.onPasswordVisibilityClick}
+                        name={'current_password'}/>
+                    <FormFieldSet
+                        label_name='New Password'
+                        value={verified_cashier_pw}
+                        onChange={this.onChange}
+                        name={'new_password'}/>
+                    <div className='settings__content_form__submit_container'>
+                        <FormSubmitButton value='UPDATE'/>
+                    </div>
+                </div>
             </div>
         );
     }
