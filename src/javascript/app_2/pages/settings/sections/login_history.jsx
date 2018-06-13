ba27cec44fb52958efd789cd401b6c996e968d94
Rustem Kakimov <ruskakimov@gmail.com>
2018-06-13 15:00:55 +0800

    move all data handling to model

--- src/javascript/app_2/pages/settings/sections/login_history.jsx
+++ src/javascript/app_2/pages/settings/sections/login_history.jsx
@@ -35,124 +35,23 @@ class LoginHistory extends PureComponent {
     }
 
     render() {
-        const { title, content, data_source } = this.props;
-        return (
-            <div>
-                {JSON.stringify(this.props.data)}
-            </div>
-        );
+        const { title, content, data, is_loading } = this.props;
+        if (!data) {
+            return <div>loading...</div>
+        }
         return (
             <div className='settings__content_container settings__login_history'>
                 <SettingContentHeader title={title} content={content}/>
-                <DataTable data_source={data_source} columns={columns} />
+                <DataTable data_source={data} columns={columns} />
             </div>
         );
     }
 }
 
-// TODO: remove this later
-const dummy_response = [
-    {
-      "action": "login",
-      "environment": "23-Apr-18 02:49:34GMT IP=61.6.9.56 IP_COUNTRY=MY User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=EN",
-      "status": 1,
-      "time": 1524451775
-    },
-    {
-      "action": "login",
-      "environment": "18-Apr-18 03:16:25GMT IP=61.6.9.56 IP_COUNTRY=MY User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=EN",
-      "status": 1,
-      "time": 1524021386
-    },
-    {
-      "action": "login",
-      "environment": "12-Apr-18 07:28:01GMT IP=61.6.9.56 IP_COUNTRY=MY User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=EN",
-      "status": 1,
-      "time": 1523518081
-    },
-    {
-      "action": "logout",
-      "environment": "4-Apr-18 09:58:16GMT IP=61.6.9.56 IP_COUNTRY=KZ User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=EN",
-      "status": 1,
-      "time": 1522835897
-    },
-    {
-      "action": "login",
-      "environment": "4-Apr-18 09:56:58GMT IP=61.6.9.56 IP_COUNTRY=MY User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=EN",
-      "status": 1,
-      "time": 1522835818
-    },
-    {
-      "action": "logout",
-      "environment": "4-Apr-18 08:05:42GMT IP=61.6.9.56 IP_COUNTRY=KZ User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=RU",
-      "status": 1,
-      "time": 1522829143
-    },
-    {
-      "action": "login",
-      "environment": "4-Apr-18 08:03:41GMT IP=61.6.9.56 IP_COUNTRY=MY User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=RU",
-      "status": 1,
-      "time": 1522829021
-    },
-    {
-      "action": "logout",
-      "environment": "4-Apr-18 08:01:38GMT IP=61.6.9.56 IP_COUNTRY=KZ User_AGENT=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 LANG=RU",
-      "status": 1,
-      "time": 1522828898
-    }
-];
-
-
-// TODO: move parsing functions somewhere else
-// (copied from iphistory.data.js)
-
-const parseUserAgent = (user_agent) => {
-    // Table of UA-values (and precedences) from:
-    //  https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent
-    // Regexes stolen from:
-    //  https://github.com/biggora/express-useragent/blob/master/lib/express-useragent.js
-    const lookup = [
-        { name: 'Edge',      regex: /Edge\/([\d\w.-]+)/i },
-        { name: 'SeaMonkey', regex: /seamonkey\/([\d\w.-]+)/i },
-        { name: 'Opera',     regex: /(?:opera|opr)\/([\d\w.-]+)/i },
-        { name: 'Chromium',  regex: /(?:chromium|crios)\/([\d\w.-]+)/i },
-        { name: 'Chrome',    regex: /chrome\/([\d\w.-]+)/i },
-        { name: 'Safari',    regex: /version\/([\d\w.-]+)/i },
-        { name: 'IE',        regex: /msie\s([\d.]+[\d])/i },
-        { name: 'IE',        regex: /trident\/\d+\.\d+;.*[rv:]+(\d+\.\d)/i },
-        { name: 'Firefox',   regex: /firefox\/([\d\w.-]+)/i },
-    ];
-    for (let i = 0; i < lookup.length; i++) {
-        const info  = lookup[i];
-        const match = user_agent.match(info.regex);
-        if (match !== null) {
-            return {
-                name   : info.name,
-                version: match[1],
-            };
-        }
-    }
-    return null;
-};
-
-const parse = (activity) => ({
-    time   : `${moment.unix(activity.time).utc().format('YYYY-MM-DD HH:mm:ss')} GMT`,
-    action : localize(activity.action),
-    success: activity.status === 1 ? localize('Successful') : localize('Failed'),
-    browser: (() => {
-        const browser = parseUserAgent(activity.environment.match('User_AGENT=(.+) LANG')[1]);
-        return `${browser.name} v${browser.version}`;
-    })(),
-    ip_addr: activity.environment.split(' ')[2].split('=')[1],
-});
-
-// LoginHistory.defaultProps = {
-//     data_source: dummy_response.map(parse),
-// };
-
 export default connect(
-    ({ main: {settings} }) => ({
-        data: settings.login_history.data,
-        getData: settings.login_history.getData,
+    ({ main: {settings: {login_history} } }) => ({
+        data: login_history.data,
+        getData: login_history.getData,
+        is_loading: login_history.is_loading,
     })
 )(LoginHistory);
--- src/javascript/app_2/store/models/settings/sections/login_history_model.js
+++ src/javascript/app_2/store/models/settings/sections/login_history_model.js
@@ -1,5 +1,7 @@
 import { observable, action } from 'mobx';
+import moment from 'moment';
 import DAO from '../../../../data/dao';
+import { localize } from '../../../../../_common/localize';
 
 export default class LoginHistoryModel {
     @observable data       = null;
@@ -11,9 +13,53 @@ export default class LoginHistoryModel {
         this.is_loading = true;
 
         DAO.getLoginHistory(this.limit).then(response => {
-            this.data = response;
-            console.log(this.data);
+            console.log(response);
+            this.data = response.login_history.map(parse);
             this.is_loading = false;
         });
     }
 }
+
+/*
+    RESPONSE PARSING FUNCTIONS
+    (copied from iphistory.data.js)
+*/
+const parseUserAgent = (user_agent) => {
+    // Table of UA-values (and precedences) from:
+    //  https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent
+    // Regexes stolen from:
+    //  https://github.com/biggora/express-useragent/blob/master/lib/express-useragent.js
+    const lookup = [
+        { name: 'Edge',      regex: /Edge\/([\d\w.-]+)/i },
+        { name: 'SeaMonkey', regex: /seamonkey\/([\d\w.-]+)/i },
+        { name: 'Opera',     regex: /(?:opera|opr)\/([\d\w.-]+)/i },
+        { name: 'Chromium',  regex: /(?:chromium|crios)\/([\d\w.-]+)/i },
+        { name: 'Chrome',    regex: /chrome\/([\d\w.-]+)/i },
+        { name: 'Safari',    regex: /version\/([\d\w.-]+)/i },
+        { name: 'IE',        regex: /msie\s([\d.]+[\d])/i },
+        { name: 'IE',        regex: /trident\/\d+\.\d+;.*[rv:]+(\d+\.\d)/i },
+        { name: 'Firefox',   regex: /firefox\/([\d\w.-]+)/i },
+    ];
+    for (let i = 0; i < lookup.length; i++) {
+        const info  = lookup[i];
+        const match = user_agent.match(info.regex);
+        if (match !== null) {
+            return {
+                name   : info.name,
+                version: match[1],
+            };
+        }
+    }
+    return null;
+};
+
+const parse = (activity) => ({
+    time   : `${moment.unix(activity.time).utc().format('YYYY-MM-DD HH:mm:ss')} GMT`,
+    action : localize(activity.action),
+    success: activity.status === 1 ? localize('Successful') : localize('Failed'),
+    browser: (() => {
+        const browser = parseUserAgent(activity.environment.match('User_AGENT=(.+) LANG')[1]);
+        return `${browser.name} v${browser.version}`;
+    })(),
+    ip_addr: activity.environment.split(' ')[2].split('=')[1],
+});
