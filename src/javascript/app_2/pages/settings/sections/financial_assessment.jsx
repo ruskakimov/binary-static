cb00535d1fb7968cc45c02499f4f1a4614a6ad97
John Baek <jbaek7023@gmail.com>
2018-06-14 10:30:03 +0800

    refactoring financial aseessment done

--- src/javascript/app_2/pages/settings/sections/financial_assessment.jsx
+++ src/javascript/app_2/pages/settings/sections/financial_assessment.jsx
@@ -4,47 +4,23 @@ import { FormSubmitButton } from '../components/form_submit_button.jsx';
 import { FormFieldSetList } from '../components/form_field_set_list.jsx';
 import Loading from '../../../../../templates/_common/components/loading.jsx';
 import DAO from '../../../data/dao';
+import { connect } from '../../../store/connect';
 
 class FinancialAssessment extends PureComponent {
-    state = {
-        is_loading: true,
-        income_source: '',
-        employment_status: '',
-        employment_industry: '',
-        occupation: '',
-        source_of_wealth: '',
-        education_level: '',
-        net_income: '',
-        estimated_worth: '',
-        account_turnover: '',
-    }
-
     async componentDidMount() {
-        let { get_financial_assessment } = await DAO.getFinancialAssessment()
-        if( get_financial_assessment ) {
-            get_financial_assessment.is_loading = false;
-            this.setState(get_financial_assessment);
-        } else {
-            // To-Do: Show the error page.
-            console.log('You must login to see this page');
-        }
-    }
-
-    onChange = (e) => {
-        const { name, value } = e.target;
-        this.setState({[name]: value})
+        this.props.getFinancialAssessment();
     }
 
     render() {
-        const { title, content } = this.props;
+        const { title, content, is_loading, onChange, data } = this.props;
         return (
             <div className='settings__content_container'>
                 <SettingContentHeader title={title} content={content}/>
                 {
-                    this.state.is_loading ?
+                    is_loading ?
                     <Loading /> :
                     <div className='settings__content_form__container'>
-                        <FormFieldSetList data={this.state} onChange={this.onChange}/>
+                        <FormFieldSetList data={data} onChange={onChange}/>
                         <div className='settings__content_form__submit_container'>
                             <FormSubmitButton value='UPDATE'/>
                         </div>
@@ -55,4 +31,11 @@ class FinancialAssessment extends PureComponent {
     }
 }
 
-export default FinancialAssessment;
+export default connect(
+    ({ main: { settings: { financial_assessment } } }) => ({
+        is_loading            : financial_assessment.is_loading,
+        data                  : financial_assessment.data,
+        getFinancialAssessment: financial_assessment.getFinancialAssessment,
+        onChange              : financial_assessment.onChange,
+    })
+)(FinancialAssessment);
---
+++ src/javascript/app_2/store/models/settings/sections/financial_assessment_model.js
@@ -0,0 +1,58 @@
+import { observable, action } from 'mobx';
+import DAO from '../../../../data/dao';
+
+export default class SelfExclusionModel {
+    @observable is_loading = true;
+
+    @observable data = {
+        income_source      : '',
+        employment_status  : '',
+        employment_industry: '',
+        occupation         : '',
+        source_of_wealth   : '',
+        education_level    : '',
+        net_income         : '',
+        estimated_worth    : '',
+        account_turnover   : '',
+    }
+
+    @action.bound
+    async getFinancialAssessment() {
+        const { get_financial_assessment } = await DAO.getFinancialAssessment()
+        if ( get_financial_assessment ) {
+            this.is_loading = false;
+            Object.entries(this.data).forEach(
+                ([k,v]) =>
+                    this.data[k] = get_financial_assessment[k] ?
+                    get_financial_assessment[k] :
+                    v);
+            this.is_loading = false;
+        } else {
+            // To-Do: Show the error page.
+            console.log('You must login to see this page');
+        }
+    }
+
+    @action.bound
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // DAO.setSelfExclusion(
+        //     this.data,
+        //     ()=> {
+        //         // To-Do: Show Success page.
+        //         console.log('successfully self-excluted!');
+        //     },
+        //     (message) => {
+        //         // To-Do: Show the error page.
+        //         // To-DO: Validation: timeout_until.
+        //         console.log(message);
+        //     },
+        // );
+    }
+
+    @action.bound
+    onChange = (e) => {
+        const { name, value } = e.target;
+        this.data[name] = value;
+    }
+}
