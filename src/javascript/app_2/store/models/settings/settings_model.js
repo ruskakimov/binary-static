561863d3d1b37831188a147000b238dc3186c32b
John Baek <jbaek7023@gmail.com>
2018-06-14 11:17:14 +0800

    Account PW refactor done

--- src/javascript/app_2/store/models/settings/settings_model.js
+++ src/javascript/app_2/store/models/settings/settings_model.js
@@ -3,6 +3,7 @@ import LoginHistoryModel from './sections/login_history_model';
 import SelfExclusionModel from './sections/self_exclusion_model';
 import FinancialAssessmentModel from './sections/financial_assessment_model';
 import CashierPasswordModel from './sections/cashier_password_model';
+import AccountPasswordModel from './sections/account_password_model';
 
 export default class SettingsModel {
     constructor() {
@@ -10,5 +11,6 @@ export default class SettingsModel {
         this.self_exclusion = new SelfExclusionModel();
         this.financial_assessment = new FinancialAssessmentModel();
         this.cashier_password = new CashierPasswordModel();
+        this.account_password = new AccountPasswordModel();
     }
 }
