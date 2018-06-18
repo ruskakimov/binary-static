771a986ffb9095c858677bedc4089e322475fdfd
John Baek <jbaek7023@gmail.com>
2018-06-18 14:17:09 +0800

    added api token help page

--- src/javascript/app_2/pages/settings/sections/api_token.jsx
+++ src/javascript/app_2/pages/settings/sections/api_token.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { SettingContentHeader } from '../components/setting_content_header.jsx';
 import { FormSubmitButton } from '../components/form_submit_button.jsx';
 import { FormFieldSetList } from '../components/form_field_set_list.jsx';
+import { SettingsHelpContainer } from '../components/settings_help_container.jsx';
 import CheckBox from '../components/check_box.jsx';
 import { connect } from '../../../store/connect';
 
@@ -46,17 +47,28 @@ class ApiToken extends PureComponent {
         return (
             <div className='settings__content_container'>
                 <SettingContentHeader title={title} content={content}/>
-                <div className='settings__content_form__container'>
-                    <div className='settings__content_sub_title__container'>
-                        <p className='settings__content_sub_title__text'>Create New token</p>
-                    </div>
-                    <FormFieldSetList data={data} onChange={onChange}/>
-                        <div>
-                            <label className='settings__content_form__input_label'>Choose Scopes</label>
+                <div className='settings__container'>
+                    <div className='settings__content_form__container'>
+                        <div className='settings__content_sub_title__container'>
+                            <p className='settings__content_sub_title__text'>Create New token</p>
                         </div>
-                        {this.createCheckBoxes()}
-                    <div className='settings__content_form__submit_container'>
-                        <FormSubmitButton value='CREATE'/>
+                        <FormFieldSetList data={data} onChange={onChange}/>
+                            <div>
+                                <label className='settings__content_form__input_label'>Choose Scopes</label>
+                            </div>
+                            {this.createCheckBoxes()}
+                        <div className='settings__content_form__submit_container'>
+                            <FormSubmitButton value='CREATE'/>
+                        </div>
+                    </div>
+                    <div>
+                        <SettingsHelpContainer>
+                            <p className='settings__help_head_text'>How to use API Token</p>
+                            <ul className='settings__help_list'>
+                                <li className='settings__help_text'>Simply click on “Create” to generate your token; then copy and paste it into the app.</li>
+                                <li className='settings__help_text'>Choose the specific type of API token that you need, based on the capabilities that you wish to make available.</li>
+                            </ul>
+                        </SettingsHelpContainer>
                     </div>
                 </div>
             </div>
