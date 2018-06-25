fa4e28ce353625b8d27d25daa19a20514c6d95a1
zakibinary <zaki@binary.com>
2018-06-25 11:44:53 +0800

    fix portfolio drawer toggle function

--- src/javascript/app_2/store/ui_store.js
+++ src/javascript/app_2/store/ui_store.js
@@ -6,7 +6,7 @@ export default class UIStore {
     @observable is_notifications_drawer_on = false;
 
     @action.bound togglePortfolioDrawer() { // show and hide Portfolio Drawer
-        this.is_portfolio_drawer_on = true;
+        this.is_portfolio_drawer_on = !this.is_portfolio_drawer_on;
     };
 
     @action.bound showMainDrawer() { // show main Drawer
