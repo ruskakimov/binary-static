418977881efb1cd3218bb4000080ef552cdd0c70
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-10 10:36:33 +0800

    Deploy to mt5_fixes

--- br_mt5_fixes/js/manifest.js
+++ br_mt5_fixes/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"38bc51524bbbe55c57d5","1":"25527f06b4571fc00b34","2":"6d7a7488cfe73566ebe6","3":"503514944d1d45fecc28"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"38bc51524bbbe55c57d5","1":"25527f06b4571fc00b34","2":"d7ead8a599ece26a1470","3":"503514944d1d45fecc28"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
