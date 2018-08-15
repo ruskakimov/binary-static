c9890647e95b52318c94c9bc34a84fcfa5535699
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-15 15:15:13 +0800

    Deploy to proposal_display_fix

--- br_proposal_display_fix/js/manifest.js
+++ br_proposal_display_fix/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"38bc51524bbbe55c57d5","1":"25527f06b4571fc00b34","2":"a1d6365583614aabdd98","3":"503514944d1d45fecc28"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"38bc51524bbbe55c57d5","1":"25527f06b4571fc00b34","2":"e0bb977315fdc00dcb23","3":"503514944d1d45fecc28"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
